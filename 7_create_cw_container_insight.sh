3618ef3824d1d95356b50b7bad2252da6b10e952
haozhx23 <121946073+haozhx23@users.noreply.github.com>
2025-03-17 19:46:29 +0800

    Update and rename 7_create_cw_container_insight.sh to 7_create_plugins.sh

--- 7_create_cw_container_insight.sh
+++
@@ -1,5 +0,0 @@
-aws iam attach-role-policy \
---role-name $SMHP_CLUSTER_ROLE \
---policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
-
-aws eks create-addon --addon-name amazon-cloudwatch-observability --cluster-name $EKS_CLUSTER_NAME
---
+++ 7_create_plugins.sh
@@ -0,0 +1,9 @@
+aws iam attach-role-policy \
+--role-name $SMHP_CLUSTER_ROLE \
+--policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
+
+aws eks create-addon --addon-name amazon-cloudwatch-observability --cluster-name $EKS_CLUSTER_NAME
+
+aws eks create-addon --region $AWS_REGION --cluster-name $EKS_CLUSTER_NAME --addon-name amazon-sagemaker-hyperpod-taskgovernance
+
+aws eks describe-addon --region $AWS_REGION --cluster-name $EKS_CLUSTER_NAME --addon-name amazon-sagemaker-hyperpod-taskgovernance
