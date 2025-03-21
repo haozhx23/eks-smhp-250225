1f2581dcc0946ff39a7845710f75472fc5bed402
Ubuntu <ubuntu@ip-10-9-0-93.ec2.internal>
2025-03-21 01:59:47 +0000

    add

--- 7_create_plugins.sh
+++ 7_create_plugins.sh
@@ -1,9 +1,9 @@
 aws iam attach-role-policy \
---role-name $SMHP_CLUSTER_ROLE \
+--role-name $SMHP_CLUSTER_EXEC_ROLE \
 --policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
 
 aws eks create-addon --addon-name amazon-cloudwatch-observability --cluster-name $EKS_CLUSTER_NAME
 
 aws eks create-addon --region $AWS_REGION --cluster-name $EKS_CLUSTER_NAME --addon-name amazon-sagemaker-hyperpod-taskgovernance
 
-aws eks describe-addon --region $AWS_REGION --cluster-name $EKS_CLUSTER_NAME # --addon-name amazon-sagemaker-hyperpod-taskgovernance
+aws eks describe-addon --region $AWS_REGION --cluster-name $EKS_CLUSTER_NAME --addon-name amazon-sagemaker-hyperpod-taskgovernance
